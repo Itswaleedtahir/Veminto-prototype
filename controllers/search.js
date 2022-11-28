c72833dd46f14b8feb28267a896999715f939b9a
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-11-28 11:53:10 +0500

    changed

--- controllers/search.js
+++ controllers/search.js
@@ -14,31 +14,17 @@ module.exports =  {
           whereClause.Firmanavn[Op.or].push({
             [Op.like]: `%${searchTerm}%`
             })
-        }
-        
-            }
+        }}
             const rq = { ...whereClause , virksomhedsform ,branchebetegnelse_primær,ansatte, antalPenheder,yearly_result }
             const OrCond = [];
             for (var k in rq){
                 if (rq.hasOwnProperty(k)) {
                     if(rq[k]) OrCond.push({[k]: rq[k]});
-                }
-            }
+                }}
             console.log(OrCond);
         //  if (!Firmanavn || !virksomhedsform ||!branchebetegnelse_primær ||!ansatte ||!antalPenheder ||!yearly_result) {
         //   throw { status: 400, message: "Required field cannot be empty." };
         // } 
-    //     const searchTerms = Firmanavn.split();     
-    //      const whereClause = {Firmanavn:{
-    //      [Op.or]:[]
-    //     }};
-    //   for( let searchTerm of searchTerms  ) {
-    //       whereClause.Firmanavn[Op.or].push({
-    //         [Op.like]: `%${searchTerm}%`
-    //         })
-    //     }
-    // buffer.toString('utf8')
-
         const comp = await CompanyData.findAll({
             where:{
                [Op.or]:[OrCond]
